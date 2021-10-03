8638db89eb772c08c82cb88113f8709f000f630f
David <davidmunyiri2@gmail.com>
2021-10-04 02:58:46 +0300

    Modified the index view

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  .models import Category, Page
 
 
 def index(request):
+    # Querying the database for for a list of all categories
+    # Odering the category list in descending order
+    # Retrieving only to 5 categories
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context = {'categories': category_list}
+
     # A dictionary to pass to the template engine
     context = {'boldmessage':'I am a bold font of the context  '}
     # Rendering a response to the client
