fb3e7e8bc41b55ea01d21c57343e573a2ec5a7da
David <davidmunyiri2@gmail.com>
2021-10-04 02:51:15 +0300

    added views n likes in category model

--- tango/populate.py
+++ tango/populate.py
@@ -9,7 +9,7 @@ from rango.models import  Page, Category
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', views=128, likes=64)
 
     add_page(cat=python_cat, title="Official Python Tutorial", url="py.org")
 
@@ -17,7 +17,7 @@ def populate():
 
     add_page(cat=python_cat, title="Official Python Tutorial By David", url="pythonbasics.org")
 
-    django_cat = add_cat('Django')
+    django_cat = add_cat('Django', views=64, likes=32)
 
     add_page(cat=django_cat, title="Django Basics Tutorial", url="djangobasics.org")
 
@@ -25,7 +25,7 @@ def populate():
 
     add_page(cat=django_cat, title="Django 2.oTutorial", url="django2.org")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
 
     add_page(cat=frame_cat, title="flask", url="flask.org")
 
@@ -38,20 +38,18 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1} ".format(str(c), str(p)))
 
+
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 
 # start execution here
 if __name__ == '__main__':
     print("Starting Rango population Script")
-
-
-
-
     populate()
--- tango/rango/admin.py
+++ tango/rango/admin.py
@@ -9,6 +9,10 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
